1f929260c770e29da469b6acc8ff31c347455ba9
gps13 <g.psistakis@gmail.com>
2013-07-05 09:33:42 +0000

    changed web.js to use buffer and read from index.html

--- web_orig.js
+++ web_orig.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
